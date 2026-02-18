0d22c9e9e4d5cb2c9a283be6c073d38178b20801
in0ix7 <in0ix7@gmail.com>
2026-02-18 23:19:32 +0300

    Initial commit: basic Spring Boot project with Docker

---
+++ src/main/java/com/healthsync/healthsync_backend/HealthsyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.healthsync.healthsync_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthsyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthsyncBackendApplication.class, args);
+	}
+
+}
